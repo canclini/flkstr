56a7f7b64ecea398aead63beddf1427ad524ce4c
canclini <marcel.canclini@me.com>
2010-10-09 14:07:36 +0200

    messages are now adapted to the new style

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
    
   def index
     @messages = current_company.received_messages.paginate :per_page => 10, :page => params[:page], :order => 'updated_at DESC'
+    @active = "in"
   end
   
   def actionbox
@@ -11,14 +12,21 @@ class MessagesController < ApplicationController
   
   def sent
     @messages = current_company.sent_messages.paginate :per_page => 10, :page => params[:page], :order => 'created_at DESC'
+    @active = "out"
   end
   
   def show
     @message = Message.find(params[:id])
+    if @message.author == current_company
+      @active = "out"
+    else
+      @active = "in"
+    end
   end
   
   def new
     @message = current_company.sent_messages.build
+    @active = "create"
   end
   
   def reply
@@ -27,6 +35,7 @@ class MessagesController < ApplicationController
     subject = @original.subject.sub(/^(Re: )?/, "Re: ")
     body = @original.body.gsub(/^/, "> ")
     @message = current_company.sent_messages.build(:to => @original.author.name, :subject => subject, :body => body)
+    @active = "create"
   end  
   
   def create
@@ -37,6 +46,7 @@ class MessagesController < ApplicationController
       flash[:notice] = "Message sent."
       redirect_to messages_path()
     else
+      @active = "create"
       render :action => "new"
     end
   end
