cda720efbe30304b43cdaf20f48400707df6a4cb
Marcel Canclini <marcel.canclini@me.com>
2010-10-14 11:53:18 +0200

    moved migration request status

--- db/migrate/20101008154428_add_status_to_requests.rb
+++
@@ -1,9 +0,0 @@
-class AddStatusToRequests < ActiveRecord::Migration
-  def self.up
-    add_column :requests, :status, :string, :default => 'open'
-    execute 'UPDATE requests SET status="open"'
-  end
-  def self.down
-    remove_colum :requests, :status
-  end
-end
