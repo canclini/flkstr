f3a09082fdb0c4a3acf14ca2e447bd8e58e4374e
Marcel Canclini <marcel.canclini@me.com>
2010-10-14 11:23:42 +0200

    removed random order in with_logo scope

--- app/models/company.rb
+++ app/models/company.rb
@@ -6,7 +6,7 @@ class Company < ActiveRecord::Base
   has_attached_file :logo, :styles => { :medium => "250x250>", :thumb => "50x50#" }
 #  validates_attachment_size :logo, :less_than => 1.megabytes
 #  validates_attachment_content_type :logo, :content_type => ['image/jpeg', 'image/png']
-  scope :with_logo, where("logo_file_name <> ?",'false').limit(10).order('rand()')
+  scope :with_logo, where("logo_file_name <> ?",'false').limit(10)#.order('rand()')
 
   has_one :setting, :dependent => :destroy
   has_many :users
