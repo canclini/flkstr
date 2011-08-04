1bcbf9c107ca31bc6ce963d26467ce85be8de6e6
Marcel Canclini <marcel.canclini@gmail.com>
2011-08-04 15:30:29 +0200

    added spec for mailers

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -15,7 +15,7 @@ class UserMailer < ActionMailer::Base
   
   def password_reset(user)
     @user = user
-    mail :to => user.email, :subject => "Password reset"
+    mail :to => user.email, :subject => "Password Reset"
   end
   
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,4 +1,28 @@
 require "spec_helper"
 
 describe UserMailer do
+  describe "password reset" do
+    let(:user) { Factory(:user, :password_reset_token => "anything") }
+    let(:mail) { UserMailer.password_reset(user) }
+
+    it "send user password reset url" do
+      mail.subject.should eq("Password Reset")
+      mail.to.should eq([user.email])
+      mail.from.should eq(["register@flockstreet.com"])
+      mail.body.encoded.should match(edit_password_reset_path(user.password_reset_token))
+    end
+  end
+  
+  describe "registration confirmation" do
+    let(:user) { Factory(:user) }
+    let(:mail) { UserMailer.registration_confirmation(user) }
+
+    it "send user password reset url" do
+      mail.subject.should eq("Erfolgreich registriert!")
+      mail.to.should eq([user.email])
+      mail.from.should eq(["register@flockstreet.com"])
+      mail.body.encoded.should match(user.company.name)
+    end
+  end
+  
 end
