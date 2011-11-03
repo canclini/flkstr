91abe73ed9a1e3ad1470b3cb62015b44d15e6044
Carosch <schnell.caroline@gmail.com>
2011-11-03 22:08:29 +0100

    installation dev environment caroline

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# -*- encoding : utf-8 -*-
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -220,32 +220,6 @@ ActiveRecord::Schema.define(:version => 20110922210109) do
     t.string "name"
   end
 
-  create_table "tolk_locales", :force => true do |t|
-    t.string   "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "tolk_locales", ["name"], :name => "index_tolk_locales_on_name", :unique => true
-
-  create_table "tolk_phrases", :force => true do |t|
-    t.text     "key"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "tolk_translations", :force => true do |t|
-    t.integer  "phrase_id"
-    t.integer  "locale_id"
-    t.text     "text"
-    t.text     "previous_text"
-    t.boolean  "primary_updated", :default => false
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "tolk_translations", ["phrase_id", "locale_id"], :name => "index_tolk_translations_on_phrase_id_and_locale_id", :unique => true
-
   create_table "updates", :force => true do |t|
     t.integer  "company_id"
     t.string   "message"
@@ -255,13 +229,7 @@ ActiveRecord::Schema.define(:version => 20110922210109) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "email",                  :default => "",       :null => false
-    t.string   "reset_password_token"
-    t.integer  "sign_in_count",          :default => 0
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
+    t.string   "email"
     t.string   "firstname"
     t.string   "lastname"
     t.string   "language"
@@ -286,6 +254,5 @@ ActiveRecord::Schema.define(:version => 20110922210109) do
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
